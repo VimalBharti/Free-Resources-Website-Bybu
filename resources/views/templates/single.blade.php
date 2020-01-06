fd16c8e716d9b13c70b634ed876c5dffe9a36089
GetSwastik <Getswastik@gmail.com>
2020-01-06 21:31:01 +0530

    changes

--- resources/views/templates/single.blade.php
+++ resources/views/templates/single.blade.php
@@ -20,25 +20,7 @@
       <h2>{{$template->title}}</h2>
       <p>{{$template->about}}</p>
 
-      <v-btn
-        color="black"
-        block 
-        dark
-        large
-        outlined
-        href="{{route('template-preview', $template->slug)}}"
-      >
-        Preview 
-      </v-btn>
-
-      <v-btn
-        color="#A2316B"
-        block dark
-        large
-        href="/storage/templates/bootstrap/{{$template->bootstrap}}"
-      >
-        Download 
-      </v-btn>
+      
 
     </v-flex>
   </v-layout>
